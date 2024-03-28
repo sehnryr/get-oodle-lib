384f10a948ed114edf29c9d65d5193c5714a030e
sehnryr <youn@melois.dev>
2024-03-28 02:36:02 +0100

    perf: :zap: use dedicated parsing library
    fast binary parsing

--- get_oodle_lib/__main__.py
+++ get_oodle_lib/__main__.py
@@ -1,18 +1,9 @@
-import gzip
 import os
-import urllib.request
-import xml.etree.ElementTree as etree
 from pathlib import Path
 
+from parse_commit_gitdeps_xml import CommitGitdepsXML
+
 from get_oodle_lib.common import parser
-from get_oodle_lib.parse import (
-    get_blobs,
-    get_files,
-    get_libraries,
-    get_packs,
-    match_blobs,
-    match_packs,
-)
 
 
 def main():
@@ -32,51 +23,48 @@ def main():
             print("Invalid path to output directory")
             exit(1)
 
-    # Read the file and parse it
-    with open(args.gitdeps, "r") as f:
-        tree = etree.parse(f)
+    platform_map = {
+        "windows": "Win64",
+        "linux": "Linux",
+        "mac": "Mac",
+    }
 
-    base_url = tree.getroot().attrib["BaseUrl"]
+    # Parse the Commit.gitdeps.xml
+    with CommitGitdepsXML(args.gitdeps) as gitdeps:
+        file_prefix = b"Engine/Source/Runtime/OodleDataCompression/Sdks/"
+        file_paths = gitdeps.find_file_names(file_prefix)
 
-    # Get the files
-    files = get_files(tree)
-    libraries = get_libraries(files, args.platform)
+        get_version = lambda name: tuple(
+            map(int, name[len(file_prefix) :].split(b"/", 1)[0].split(b"."))
+        )
 
-    # Get the blobs
-    blobs = get_blobs(tree)
-    blobs = match_blobs(libraries, blobs)
+        # Get the latest version of the library
+        versions = [get_version(file_path) for file_path in file_paths]
+        latest_version = max(versions)
+        latest_version_text = ".".join(map(str, latest_version)).encode()
 
-    # Get the packs
-    packs = get_packs(tree)
-    packs = match_packs(blobs, packs)
+        # Get the latest files
+        latest_files = [
+            file_path
+            for file_path in file_paths
+            if file_path[len(file_prefix) :].startswith(latest_version_text)
+        ]
 
-    # Download the libraries
-    for library_path, library_data in libraries.items():
-        file_name = library_path.rsplit("/", 1)[-1]
+        # Get the libraries for the target platform
+        target_libraries = [
+            file_path
+            for file_path in latest_files
+            if file_path[
+                len(file_prefix) + len(latest_version_text) + 1 :
+            ].startswith(b"lib/" + platform_map[args.platform].encode() + b"/")
+        ]
 
-        blob_hash = library_data["hash"]
-        blob = blobs[blob_hash]
+        for file_path in target_libraries:
+            file_name = file_path.rsplit(b"/", 1)[-1].decode()
 
-        size = int(blob["size"])
-
-        pack_offset = int(blob["pack_offset"])
-        pack_hash = str(blob["pack_hash"])
-        pack = packs[pack_hash]
-
-        remote_path = pack["remote_path"]
-        url = f"{base_url}/{remote_path}/{pack_hash}"
-
-        # Download the pack
-        response = urllib.request.urlopen(url)
-        compressed = response.read()
-
-        # Decompress the pack
-        decompressed = gzip.decompress(compressed)
-
-        # Write the library
-        with open(f"{args.output}/{file_name}", "wb") as f:
-            print(f"Writing {file_name}...")
-            f.write(decompressed[pack_offset : pack_offset + size])
+            with open(f"{args.output}/{file_name}", "wb") as f:
+                print(f"Writing {file_name}...")
+                f.write(gitdeps.fetch_file(file_path))
 
 
 if __name__ == "__main__":
